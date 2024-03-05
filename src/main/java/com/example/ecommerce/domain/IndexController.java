dc34c30939049a69ebad9a9694dea212a8a7adfa
yoni <kkt1692@konkuk.ac.kr>
2024-03-06 01:29:21 +0900

    [fix] : 의존성 문제 해결

--- src/main/java/com/example/ecommerce/domain/IndexController.java
+++ src/main/java/com/example/ecommerce/domain/IndexController.java
@@ -1,10 +1,6 @@
 package com.example.ecommerce.domain;
 
-import com.example.ecommerce.domain.user.dto.request.UserCreateRequestDto;
-import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
