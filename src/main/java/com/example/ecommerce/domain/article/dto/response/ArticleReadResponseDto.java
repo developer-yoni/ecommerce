615d76d792b6de917dbd28ab2e01fe75ee43e9f8
yoni <kkt1692@konkuk.ac.kr>
2024-03-07 23:57:30 +0900

    [feat] : LocalDateTime과 Date간 시간 변환에 따른 응답 추가

--- src/main/java/com/example/ecommerce/domain/article/dto/response/ArticleReadResponseDto.java
+++ src/main/java/com/example/ecommerce/domain/article/dto/response/ArticleReadResponseDto.java
@@ -2,9 +2,13 @@ package com.example.ecommerce.domain.article.dto.response;
 
 import com.example.ecommerce.domain.article.Article;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,14 +32,28 @@ public class ArticleReadResponseDto {
     @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss", timezone="UTC") // ZonedDateTime은 @JsonFormat의 timezone에 영향 받아서 변환됨
     private ZonedDateTime time3;
 
+    private LocalDateTime convertedTime2;
+    private Date convertedConvertedTime2;
+    private Date convertedTime1;
+
 
     public static ArticleReadResponseDto toDto(Article article) {
+
+        // Date를 LocalDateTime으로 변환할 땐 -> 변환될 시간의 Timezone을 설정하 수 있으니 오히려 좋다 (실제 영향을 받음)
+        LocalDateTime convertedTime2 = article.getTime2().toInstant().atZone(ZoneId.of("UTC")).toLocalDateTime();
+        // [주의] : 현재 LocalDateTime을 Springboot의 timezone 시간으로 인지하고 -> 이를 UTC로 다시 변환하니 (Date기본이 UTC인듯) -> 문제가 생길 수 있다
+        Timestamp convertedConvertedTime2 = Timestamp.valueOf(convertedTime2);
+        // 그냥 LocalDateTime값을 UTC로 변환한다
+        Timestamp convertedTime1 = Timestamp.valueOf(article.getTime1());
 
         return ArticleReadResponseDto.builder()
                                      .articleId(article.getId())
                                      .time1(article.getTime1())
                                      .time2(article.getTime2())
                                      .time3(article.getTime3())
+                                     .convertedTime2(convertedTime2)
+                                     .convertedConvertedTime2(convertedConvertedTime2)
+                                     .convertedTime1(convertedTime1)
                                      .build();
     }
 }
