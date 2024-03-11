fc7d68d3721f2b47fca29bf4d7b5ab392c5be584
yoni <kkt1692@konkuk.ac.kr>
2024-03-11 12:34:08 +0900

    [feat] : Springboot Timezone을 UTC로, hibernate timezone관련 설정을 UTC로 설정 추가

--- src/main/java/com/example/ecommerce/ECommerceApplication.java
+++ src/main/java/com/example/ecommerce/ECommerceApplication.java
@@ -1,5 +1,7 @@
 package com.example.ecommerce;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,8 +10,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class ECommerceApplication {
 
+	@PostConstruct
+	public void setTimezone() {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		System.out.println("***** Springboot Timezone : " + TimeZone.getDefault());
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
-
 }
